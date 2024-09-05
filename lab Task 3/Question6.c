b1fa5b29559384d38d2884082d1e2797f630de5e
Syed Muhammad  Burair Hyder <burairhyder519@gmail.com>
2024-09-05 19:55:49 +0500

    Create Question6.c

---
+++ lab Task 3/Question6.c
@@ -0,0 +1,9 @@
+// Question6
+#include <stdio.h>
+
+int main() {
+    int x1 =5, y1 =4, x2 =3 , y2  = 2;
+    float slope;
+    slope  = (y2-y1)/(x2-x1);
+    printf("the slope of this line is %.3f",slope);
+}
