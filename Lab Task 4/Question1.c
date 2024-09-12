ff8f399b7a2501b0e06f46f2947639e0b6c697e7
Syed Muhammad  Burair Hyder <burairhyder519@gmail.com>
2024-09-12 14:49:24 +0500

    Create Question1.c

---
+++ Lab Task 4/Question1.c
@@ -0,0 +1,16 @@
+# include <stdio.h>
+int main(){
+	int num;
+	printf("please enter number ");
+	scanf("%d",&num);
+	if (num % 3== 0)
+		{printf("this is multiple of three");
+		
+		}
+	else
+		{
+		printf("this is not a multiple of three");}
+		
+	}
+	
+	
