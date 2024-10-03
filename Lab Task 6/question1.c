543a0845a608013688991fa0167d33780b03be8b
Syed Muhammad  Burair Hyder <burairhyder519@gmail.com>
2024-10-03 14:45:10 +0500

    Update question1.c

--- Lab Task 6/question1.c
+++ Lab Task 6/question1.c
@@ -14,3 +14,33 @@ int main(void){
 		b++;
 	}
 }
+
+#include <stdio.h>
+int main(){
+int a=65;
+int b=a+4;
+int i;
+for(i=0;i<=4;i++){
+
+     if(i==2){
+    	printf("       E    \n");
+    	b=72;
+    	a=69;
+    	continue;
+	}
+    else if(i==0 ){
+      printf(" %c  %c  %c  %c  %c\n",a,a+1,a+2,a+3,b);}
+    else if(i==4){
+      a=69;
+      b=73;
+      printf( " %c  %c  %c  %c  %c\n",a,a+1,a+2,a+3,b);
+	}
+    else{
+    printf("    %c     %c     \n",a,b);
+	}
+
+	a=a+2;
+	b++;
+}
+
+}
