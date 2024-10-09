13c4f761d7a405fdfa7d38d589c7cea547b03cb0
Syed Muhammad  Burair Hyder <burairhyder519@gmail.com>
2024-10-10 00:08:30 +0500

    Update question3.c

--- Lab Task 6/question3.c
+++ Lab Task 6/question3.c
@@ -1,1 +1,28 @@
 // question3 
+#include <stdio.h>
+int main() {
+    int terms;
+    printf("Enter the number of Terms: ");
+    scanf("%d", &terms);
+    int precedingterm = 0;
+    int comingterm = 1;
+    int i;
+    int sum = 0;
+    int flag = 0;
+   
+    for(i = 2; i < terms; i++){
+		if (terms%i==0)
+			flag = 1;
+	}
+	
+	if (flag==0){
+	 printf("%d %d ", precedingterm, comingterm);
+	for(i = 2; i < terms; i++){
+        sum = precedingterm + comingterm;
+        precedingterm = comingterm;
+        comingterm = sum;
+        printf("%d ", sum);
+    }
+}
+    
+}
