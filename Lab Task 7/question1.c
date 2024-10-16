be89e6963214912897c58e548d83608ff9331163
Syed Muhammad  Burair Hyder <burairhyder519@gmail.com>
2024-10-16 17:19:32 +0500

    Create question1.c

---
+++ Lab Task 7/question1.c
@@ -0,0 +1,28 @@
+# include <stdio.h>
+int main(void){
+    int arr[100];
+    int num;
+    int temp;
+    printf("how many number to enter:");
+    scanf("%d",&num);
+    for ( int  i =0;i<num;i++){
+        printf("Enter number:");
+        scanf("%d",&arr[i]);
+        
+    }
+    for (int i =(num-1);i>=0;i--){
+        if (i == (num-1)){
+            temp = arr[i];
+            
+        }
+        
+            arr[i] = arr[i-1];
+        
+            
+        }
+        arr[0]= temp;
+        
+    for (int i =0; i <num;i++){
+        printf("%d\n",arr[i]);
+    }
+}
