81cdd3912b1d9917f90ee033bfb98202e1ff5750
Syed Muhammad  Burair Hyder <burairhyder519@gmail.com>
2024-11-27 00:45:08 +0500

    Create question3.c

---
+++ Lab Task 11/question3.c
@@ -0,0 +1,59 @@
+# include <stdio.h>
+# include <string.h>
+
+int main(void){
+	FILE *file1;
+	FILE *file2;
+	file1 = fopen("check1.txt","w");
+	char name[200];
+	char check[50];
+	char new1[50];
+	
+	
+
+	for (int i=0;i<5;i++){
+		printf("Enter line:");
+		gets(name);
+		fprintf(file1,"%s\n",&name);
+		
+	}
+	fclose(file1);
+	printf("Enter the word to replace:");
+	scanf("%s",check);
+	printf("Enter the new word:");
+	scanf("%s",new1);
+	
+int x=0;
+int res;
+
+file1= fopen("check1.txt","r");
+file2 = fopen("replace.txt","w");
+
+
+while (fscanf(file1,"%s",name)!= EOF){
+	
+		 res =strcmp(name,check);
+		 
+		 if (res==0){
+		 	fprintf(file2,"%s",new1);
+		 }
+		 else{
+		 	fprintf(file2,"%s",name);
+		 }
+		 
+		 char c = fgetc(file1);
+    if (c == '\n') {
+        fprintf(file2, "\n");
+    } else if (c != EOF) {
+        fprintf(file2, " ");
+        ungetc(c, file1);  // push back the character taken out for checking new line and space
+    }
+	
+
+
+}
+
+fclose(file1);
+fclose(file2);
+}
+
