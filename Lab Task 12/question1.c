f3cd1c87afcfe30483429d6ae7444d093e9136bf
Syed Muhammad  Burair Hyder <burairhyder519@gmail.com>
2024-12-04 13:02:45 +0500

    Update question1.c

--- Lab Task 12/question1.c
+++ Lab Task 12/question1.c
@@ -1,1 +1,27 @@
-question1 
+# include <stdio.h>
+ #include <stdlib.h>
+int main(){
+
+	int num;
+	int *arr;
+	int sum;
+	int *sptr;
+	sptr =&sum;
+	printf("How many numbers:");
+	scanf("%d",&num);
+	arr = (int*)malloc(num*sizeof(int));
+	for ( int i=0;i<num;i++){
+		printf("Enter Number#%d: ",i+1);
+		scanf("%d",(arr+i));
+	}
+	
+	for ( int i=0;i<num;i++){
+		*sptr = *sptr + *(arr+i);
+		
+	}
+	
+	printf("Sum of Numbers is : %d",*sptr);
+	
+}
+
+
